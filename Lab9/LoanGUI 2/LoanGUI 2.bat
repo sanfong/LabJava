5fe9a5615da065a0e7fce99c698ad37d901753e8
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2022-04-26 16:25:48 +0700

    update

--- Lab9/LoanGUI 2/LoanGUI 2.bat
+++ Lab9/LoanGUI 2/LoanGUI 2.bat
@@ -1,1 +1,1 @@
-java --enable-preview --module-path "C:/Program Files/Java/javafx-sdk-17.0.1/lib" --add-modules=javafx.controls,javafx.fxml -jar "%~n0.jar"
+start "" javaw --enable-preview --module-path "C:/Program Files/Java/javafx-sdk-17.0.1/lib" --add-modules=javafx.controls,javafx.fxml -jar "%~n0.jar"
--- Lab9/Template/src/App.java
+++ Lab9/Template/src/App.java
@@ -1,19 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        Scene scene = new Scene(new Pane(), 300, 300);
+        Scene scene = new Scene(new javafx.scene.layout.Pane(), 600, 400);
         primaryStage.setTitle("Program");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
